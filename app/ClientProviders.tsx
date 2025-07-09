89fdedb766bb79e9f2d97ed8d585e6f8be7cca52
gemechu <gemechu@gmail.com>
2025-07-09 10:33:14 +0300

    update it

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -1,12 +1,26 @@
 // app/ClientProviders.tsx
 'use client';
-
+import { useEffect } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import ToastProvider from "./useContext/useToast";
 import GlobalProvider from '@/app/useContext/UseContext'
 import React from 'react'
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const handleChunkError = (e: ErrorEvent) => {
+      if (e.message?.includes('Loading chunk')) {
+        window.location.reload()
+      }
+    }
+
+    window.addEventListener('error', handleChunkError)
+
+    return () => {
+      window.removeEventListener('error', handleChunkError)
+    }
+  }, [])
+
   return (
     <SessionProvider>
       <ToastProvider>
