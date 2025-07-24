f6af8b02db7a022944580ca9471b70d629e410c6
gemechu <gemechujimacs@gmail.com>
2025-07-24 09:39:25 +0300

    update prisma

--- lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-import {PrismaClient} from "../lib/generated/prisma"
-const prisma=new PrismaClient()
-const globalForPrisma=global as unknown as {prisma:typeof prisma}
-if(process.env.NODE_ENV!=='production') globalForPrisma.prisma=prisma
-
-export default prisma
