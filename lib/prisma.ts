47765eda576b397b29c2291f8c666006f9ad1f91
gemechu-jima <gemechujimacs@gmail.com>
2025-08-28 10:32:15 +0300

    all change under this new branch throught upstream

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,16 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from './generated/prisma'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// Create a global variable to avoid multiple instances in development
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
-const  prisma =
+ const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['query'], // optional: logs queries in dev
   })
 
+// Only assign globally in development
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
- export default prisma
+
+export default prisma
