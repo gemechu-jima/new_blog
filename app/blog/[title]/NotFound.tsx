2774b4a61b294f292ff5a008c7ab7204b2d73565
gemechu-jima <gemechujimacs@gmail.com>
2025-08-28 16:20:02 +0300

    push on origin

--- app/blog/[title]/NotFound.tsx
+++ app/blog/[title]/NotFound.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen text-center px-4">
-      <h1 className="text-4xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
+      <h1 className="text-4xl font-bold text-orange-500 mb-4">This blog still not exist</h1>
       <p className="text-gray-600 mb-6">Sorry, the page you are looking for does not exist.</p>
       <Link href="/blog" className="text-blue-500 hover:underline">
         Go back home
